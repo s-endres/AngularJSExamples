0d480a9305d66bc50148d67899a9f994c36109ef
Sendres <sfec03@gmail.com>
2015-09-27 21:10:36 -0600

    Custom directive at index.html line 72, and Directive creation at eventModule.js.

--- app/events/eventModule.js
+++ app/events/eventModule.js
@@ -114,4 +114,43 @@
     {
         return($scope.tab);
     }
+    $scope.isTabCurrent = function (val) {
+        if (val == $scope.getTab()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    $scope.getMyClass = function (val) {
+        if (val == $scope.getTab()) {
+            return ('active');
+        } else {
+            return ('');
+        }
+    }
+
+    this.tabs = [
+	{
+	    title: "First",
+	    description: "1 is a one day event that teaches kids how to code"
+	},
+	{
+	    title: "Second",
+	    description: "2 is a one day event that teaches kids how to code"
+	},
+	{
+	    title: "Third",
+	    description: "3 is a one day event that teaches kids how to code"
+	}
+    ]
 }])
+.directive('eventItem', function () {
+    return {
+        restrict: 'E', //E = element, A = attribute, C = class, M = comment
+        templateUrl: 'partialHtml/eventItem.html',
+        controller: function ($scope) {
+            console.log('do stuff')
+        },
+        link: function ($scope, element, attrs) { }
+    }
+})
